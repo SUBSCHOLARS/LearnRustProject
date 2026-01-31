977516562f1bd678e38fa44d79d9795fb41c4078
Kota Hatanaka <kenkotan7722@gmail.com>
2026-01-31 12:07:03 +0900

    rust-analyzer worked

---
+++ RustProgram/hello_cargo/target/debug/deps/hello_cargo-00eeb78180cb09a8.d
@@ -0,0 +1,5 @@
+/Users/hatanakakouta/Dev/LearnRustProject/RustProgram/hello_cargo/target/debug/deps/hello_cargo-00eeb78180cb09a8.d: src/main.rs
+
+/Users/hatanakakouta/Dev/LearnRustProject/RustProgram/hello_cargo/target/debug/deps/libhello_cargo-00eeb78180cb09a8.rmeta: src/main.rs
+
+src/main.rs:
